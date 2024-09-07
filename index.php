4a18a05339c3d99b524b848362b2815657ab7603
admmzikr <adamzikricahyadi66@gmail.com>
2024-09-07 18:15:15 +0700

    Second commit

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
     </div>
 
     <form action="process_form.php" method="POST">
-        <h2>Contact Us</h2>
-        <label for="name">Name:</label><br>
+        <h2>Kontak Kami</h2>
+        <label for="name">Nama:</label><br>
         <input type="text" id="name" name="name" required><br><br>
 
         <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" required><br><br>
 
-        <label for="message">Message:</label><br>
+        <label for="message">Pesan:</label><br>
         <textarea id="message" name="message" rows="5" required></textarea><br><br>
 
         <input type="submit" value="Submit">
